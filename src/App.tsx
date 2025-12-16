2dc7471baaa64b69daa57878e8fa7b381714fe42
Victor Kachin <victor.kachin@gmail.com>
2025-12-17 02:10:44 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+function App() {
+
+    const tracks = [
+        {
+            id: 1,
+            title: 'Musicfan soundtrack',
+            url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'
+        },
+        {
+            id: 2,
+            title: 'Musicfan soundtrack instrumental',
+            url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'
+        },
+    ]
+
+    return (
+        <div>
+
+            <h1>Musicfan Player</h1>
+            <ul>
+                {
+                    tracks.map(
+                        (track) => {
+                            return (
+                                <li key={track.id}>
+                                    <div>{track.title}</div>
+                                    <audio
+                                        controls
+                                        src={track.url}
+                                    >
+                                    </audio>
+                                </li>
+                            )
+                        }
+                    )
+                }
+            </ul>
+
+        </div>
+    )
+}
+
+export default App
