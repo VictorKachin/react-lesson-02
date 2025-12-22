ee5e5f4e8fa0616610e876c449070514e09404a1
Victor Kachin <victor.kachin@gmail.com>
2025-12-22 22:47:34 +0300

    fix formating

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-
 const rootEl = document.getElementById('root')
-const reactRoot = createRoot (rootEl!)
+const reactRoot = createRoot(rootEl!)
 reactRoot.render(<App />)
